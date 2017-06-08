9a536805afa149d45f9d5f3d7b90a50ee387f26b
Eric Swanson <eswanson@alloscomp.com>
2017-06-08 14:59:25 -0700

    Non-working install script

--- assets/install.sh
+++ assets/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#judgement
+#supervisor.conf already exists? do not reinstall!
 if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
   exit 0
 fi
@@ -29,102 +29,83 @@ chmod +x /opt/postfix.sh
 postconf -e myhostname=$maildomain
 postconf -F '*/*/chroot = n'
 
-############
-# SASL SUPPORT FOR CLIENTS
-# The following options set parameters needed by Postfix to enable
-# Cyrus-SASL support for authentication of mail clients.
-############
-# /etc/postfix/main.cf
-postconf -e smtpd_sasl_auth_enable=yes
-postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# smtpd.conf
-cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-pwcheck_method: auxprop
-auxprop_plugin: sasldb
-mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
+# Add the myhook hook to the end of master.cf
+tee -a /etc/postfix/master.cf <<'EOF'
+myhook unix - n n - - pipe
+    flags=F user=nobody argv=/usr/local/bin/mailhook.py ${recipient} ${sender} ${size}
 EOF
-# sasldb2
-echo $smtp_user | tr , \\n > /tmp/passwd
-while IFS=':' read -r _user _pwd; do
-  echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
-done < /tmp/passwd
-chown postfix.sasl /etc/sasldb2
 
-############
-# Enable TLS
-############
-if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
-  # /etc/postfix/main.cf
-  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
-  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
-  chmod 400 /etc/postfix/certs/*.*
-  # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
-fi
+# Make SMTP use myhook
+postconf -F 'smtp/inet/command = smtpd -o content_filter=myhook:dummy'
+
+# Disable bounces
+postconf -F 'bounce/unix/command = discard'
+
+# Disable local recipient maps so nothing is dropped b/c of non-existent email
+postconf 'local_recipient_maps ='
+
+# Create myhook script
+tee /usr/local/bin/mailhook.py <<'EOF'
+#!/usr/bin/python2
+# /usr/local/bin/handle_email.py ${recipient} ${sender} ${size}
+try:
+    import json
+    import requests
+    import sys
+    import time
+
+    from mailparser import MailParser
+
+    txt = sys.stdin.read()
+
+    parser = MailParser()
+    parser.parse_from_string(txt)
+
+    # auth = (__BASIC_USERNAME__, __BASIC_PASSWORD__)
+    auth = None
+
+    # Parse the email
+    out = {
+        'recipient': sys.argv[1],
+        'sender': sys.argv[2],
+        'size': sys.argv[3],
+        'timestamp': int(time.time()),
+
+        'body': parser.body,
+        'headers': None
+    }
+
+#headers
+#subject
+#from_email
+#from_name
+#body
+#attachments
+#attachments[].data
+#attachments[].cid
+
+    requests.post(__URL__, auth=auth, json=out, timeout=91)
+except Exception as e:
+    print("Error :(   %r" % e)
+EOF
+
+# Replace the URL in the myhook script
+sed -i "s/__URL__/$webhookurl/" /usr/local/bin/handle_email.py
+chmod +x /usr/local/bin/handle_email.py
 
 #############
-#  opendkim
+## Enable TLS
 #############
-
-if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
-  exit 0
-fi
-cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
-
-[program:opendkim]
-command=/usr/sbin/opendkim -f
-EOF
-# /etc/postfix/main.cf
-postconf -e milter_protocol=2
-postconf -e milter_default_action=accept
-postconf -e smtpd_milters=inet:localhost:12301
-postconf -e non_smtpd_milters=inet:localhost:12301
-
-cat >> /etc/opendkim.conf <<EOF
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-
-Canonicalization        relaxed/simple
-
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-
-UserID                  opendkim:opendkim
-
-Socket                  inet:12301@localhost
-EOF
-cat >> /etc/default/opendkim <<EOF
-SOCKET="inet:12301@localhost"
-EOF
-
-cat >> /etc/opendkim/TrustedHosts <<EOF
-127.0.0.1
-localhost
-192.168.0.1/24
-
-*.$maildomain
-EOF
-cat >> /etc/opendkim/KeyTable <<EOF
-mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
-EOF
-cat >> /etc/opendkim/SigningTable <<EOF
-*@$maildomain mail._domainkey.$maildomain
-EOF
-chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
-chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
+#if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
+#  # /etc/postfix/main.cf
+#  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
+#  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
+#  chmod 400 /etc/postfix/certs/*.*
+#  # /etc/postfix/master.cf
+#  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+#  postconf -P "submission/inet/syslog_name=postfix/submission"
+#  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+#  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
+#  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
+#  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+#fi
