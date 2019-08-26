eef8e9969ad54271e029678f783cf6d8a5c33153
Eric Swanson <eswanson@alloscomp.com>
2019-08-26 14:14:37 -0700

    Cap emails to 1MB by default (MAX_EMAIL_SIZE env var)

--- assets/webhook.js
+++ assets/webhook.js
@@ -3,6 +3,7 @@ var simpleParser = require('mailparser').simpleParser;
 var request = require('request');
 
 var WEBHOOK_URL = '__URL__'; //NOTE: can include basic auth in url!
+var MAX_SIZE = parseInt(process.env.MAX_EMAIL_SIZE || 1000000); // Allow up to 1MB emails by default
 
 function makeRequest(options){
     return new Promise((resolve, reject)=>{
@@ -26,26 +27,32 @@ function mapToObject(map) {
     return out
 }
 
-simpleParser(process.stdin)
-    .then(mail=>{
-        var body = mail;
-        body.envelopeRecipient = process.argv[2];
-        body.envelopeSender = process.argv[3];
-        body.envelopeSize = parseInt(process.argv[4]);
-        body.headers = mapToObject(mail.headers);
-        return makeRequest({
-            uri: WEBHOOK_URL,
-            method: "POST",
-            json: true,
-            headers: {"content-type": "application/json"},
-            body: body,
+var envelopeSize = parseInt(process.argv[4]);
+if (envelopeSize <= MAX_SIZE) {
+    simpleParser(process.stdin)
+        .then(mail=>{
+            var body = mail;
+            body.envelopeRecipient = process.argv[2];
+            body.envelopeSender = process.argv[3];
+            body.envelopeSize = envelopeSize;
+            body.headers = mapToObject(mail.headers);
+            return makeRequest({
+                uri: WEBHOOK_URL,
+                method: "POST",
+                json: true,
+                headers: {"content-type": "application/json"},
+                body: body,
+            })
+            .then((response)=>{
+                if(response.statusCode !== 200)
+                    throw "received non 200 http status code from "+WEBHOOK_URL;
+                return response;
+            })
         })
-        .then((response)=>{
-            if(response.statusCode !== 200)
-                throw "received non 200 http status code from "+WEBHOOK_URL;
-            return response;
+        .catch(err=>{
+            console.error(err);
         })
-    })
-    .catch(err=>{
-        console.error(err);
-    })
+}
+else {
+    console.log("Email is too large: size=" + envelopeSize);
+}
